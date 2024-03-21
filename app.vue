f1ddd50831547c3ddfdb5125b7bc761d920560b4
郭江林 <guojianglin@szbit.cn>
2024-03-21 18:32:06 +0800

    feat: init project

---
+++ app.vue
@@ -0,0 +1,31 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+import 'reset-css'
+
+useHead({
+  htmlAttrs: {
+    lang: 'zh-CN',
+  },
+  title: '小江大浪',
+  titleTemplate: title => title !== '小江大浪' ? `${title} · 小江大浪` : title,
+  meta: [
+    { name: 'description', content: '小江大浪的个人空间 ✨' },
+  ],
+})
+</script>
+
+<style>
+html, body , #__nuxt{
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  background: #111;
+  color: white;
+  color-scheme: dark;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  要做最特别的个人空间
+</template>
+
+<script setup lang="ts">
+
+</script>
+
